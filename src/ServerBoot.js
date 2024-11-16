3544921d9b9fb3825b70cf1bf399725f94a56f04
Louis Denman <lmdenman02@gmail.com>
2024-11-16 13:49:57 +1000

    Created Server Boot and Config files.

---
+++ src/ServerBoot.js
@@ -0,0 +1,8 @@
+var {app} = require('./ServerConfig.js')
+
+require("dotenv").config()
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
