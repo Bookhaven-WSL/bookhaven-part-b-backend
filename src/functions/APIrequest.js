e0fd8051fd217b910cfa6768ba37e866ad127a8c
Louis Denman <lmdenman02@gmail.com>
2024-12-09 21:40:15 +1000

    added API request function that retrieves the key, title, author, genres and publish date of the first 20 book results from a search.

---
+++ src/functions/APIrequest.js
@@ -0,0 +1,47 @@
+async function getApiData (title) {
+
+    let formattedTitle = title.trim().toLowerCase().replace(/ /g, '+')
+
+    const response = await fetch(`https://openlibrary.org/search.json?title=${formattedTitle}`, {
+        methods: "GET"
+    })
+    if (response.ok) {
+        let largeBody = await response.json()
+        let smallBody = []
+        for (let i = 0; i < 20; i ++) {
+            smallBody.push(largeBody.docs[i])
+        }
+
+        let body = []
+        for (let book of smallBody) {
+            let key = book.key
+            let title = book.title
+            let author = book.author_name
+            let genres = book.subject
+            let publishDate = book.first_publish_year
+
+            body.push([{"Key": key}, {"Title": title}, {"Author": author}, {"Genres": genres}, {"Published Date": publishDate}])
+        }
+
+        return body
+    }
+    else {
+        if (response.status == "404") {
+            const body = {"error": "Oh dear, we couldn't find any books matching this name! Try again."}
+            return body
+        }
+        else if (response.status == "429") {
+            const body = {"error": "Unfortunately the API this service relies on has been requested too frequently in a short period of time. Please wait for more results."}
+            return body
+        }
+        else {
+            const body = {"error": "An unexpected error occured."}
+            console.log(response.status)
+            return body
+        }
+    }
+}
+
+module.exports = {
+    getApiData
+}
