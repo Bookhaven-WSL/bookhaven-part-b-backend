8c7cd186e27dafc031727559106aacbe1fe099f5
Wade <158378370+wadevenz@users.noreply.github.com>
2024-12-09 22:02:42 +1000

    fix: scripts updated

--- src/ServerBoot.js
+++ src/ServerBoot.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, async () => {
     await dbConnection()
     if (process.env.DATABASE_URL) {
-        console.log(`Server is running on MongoDB Atlas`)
+        console.log("Server is running on port" + PORT)
     }
     else {
         console.log(`Server is running on port http://localhost:${PORT}`)
