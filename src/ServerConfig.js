860952ed125b7966884e76f7ac2b5c67561dfd8b
Wade <158378370+wadevenz@users.noreply.github.com>
2024-12-12 16:43:36 +1000

    filenames and pathways changed

--- src/ServerConfig.js
+++ src/ServerConfig.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}))
 const { getApiData } = require("./functions/APIrequest.js")
 const { UserAuthValidation } = require("./functions/JWTFunctions.js")
 const AuthControllerRoutes = require("./controllers/User/AuthControllerRoutes.js")
-const bookController = require("./controllers/bookRoutes.js")
+const BookController = require("./controllers/Book/bookRoutes.js")
 const UserControllerRoutes = require("./controllers/User/UserControllerRoutes.js")
 
 let corsURLs = {
@@ -40,7 +40,7 @@ app.get("/testRoute", UserAuthValidation, async (request, response) => {
 
 app.use("/auth", AuthControllerRoutes)
 
-app.use("/", bookController)
+app.use("/", BookController)
 
 app.use("/user", UserControllerRoutes)
 
